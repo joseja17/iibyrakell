e66a300ab0c5dc3bfd0c050a89ddafc4f08ec972
Jose Jaime Carvajal Calixto <joseja17@Joses-MacBook-Pro.local>
2019-05-10 12:54:31 -0600

    fixed build

---
+++ build/precache-manifest.9720065d49ed434efc537122f6712856.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dabff94d64e7e20730f347ee88ed4186",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c992a8e3d3916d736bf",
+    "url": "/static/css/main.2b1d6c2b.chunk.css"
+  },
+  {
+    "revision": "b0d6c3b3f1c874203930",
+    "url": "/static/js/2.48ebeb33.chunk.js"
+  },
+  {
+    "revision": "2c992a8e3d3916d736bf",
+    "url": "/static/js/main.a17a0d3e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9803d9632863dc9ea303a0dda56f66c5",
+    "url": "/static/media/GT-Haptik-Bold-2.9803d963.eot"
+  },
+  {
+    "revision": "b63967f5b46cbeb3832a009522ed67e2",
+    "url": "/static/media/GT-Haptik-Bold-2.b63967f5.woff"
+  },
+  {
+    "revision": "3b09b190cfe062cb2f24dc5c28172c61",
+    "url": "/static/media/GT-Haptik-Light-2.3b09b190.woff"
+  },
+  {
+    "revision": "caf80bdb2bce7dbaa1d5ba5b40096c60",
+    "url": "/static/media/GT-Haptik-Light-2.caf80bdb.eot"
+  },
+  {
+    "revision": "000fefaaeba226cb8ec7dcccd76084cb",
+    "url": "/static/media/GT-Haptik-Medium-2.000fefaa.eot"
+  },
+  {
+    "revision": "244691775c48dc600e6382f5aca9eb01",
+    "url": "/static/media/GT-Haptik-Medium-2.24469177.woff"
+  },
+  {
+    "revision": "67332f11b93786495265a75bc3fad0ea",
+    "url": "/static/media/GT-Haptik-Medium-2.67332f11.ttf"
+  },
+  {
+    "revision": "05975d14f9a1c2d9a5d53b46c38905f6",
+    "url": "/static/media/GT-Haptik-Regular-2.05975d14.ttf"
+  },
+  {
+    "revision": "3824696316694fac6461fdee5f83d158",
+    "url": "/static/media/GT-Haptik-Regular-2.38246963.woff"
+  },
+  {
+    "revision": "ad931c767a04de19ad37d137c4a21fde",
+    "url": "/static/media/GT-Haptik-Regular-2.ad931c76.eot"
+  },
+  {
+    "revision": "86ac48ff81a89a2f6fb7fdaaac5080a4",
+    "url": "/static/media/GT-Haptik-Thin-2.86ac48ff.woff"
+  },
+  {
+    "revision": "e8f941e63968ef12fb64f8643ef23437",
+    "url": "/static/media/GT-Haptik-Thin-2.e8f941e6.eot"
+  }
+]);
